7a8dc947f1a11b5984e0d3114e4e1b923d38bb36
Rachel <rjcrter11@gmail.com>
2021-05-04 20:16:26 -0500

    adds react-final-form, lodash and recompose packages

---
+++ src/components/PostEditor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    withStyles,
+    Card,
+    CardContent,
+    CardActions,
+    Modal,
+    Button,
+    TextField
+} from '@material-ui/core';
+import { compose } from 'recompose';
+import { withRouter } from 'react-router-dom';
+import { Form, Field } from 'react-final-form';
